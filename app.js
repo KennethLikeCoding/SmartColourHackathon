92164b951abd0f9a0aa103e3e68f549d8cbc38ec
LENOVO-PC\Kinhung <kwokinhungisme@gmail.com>
2018-07-29 11:25:34 +1000

    files are restructed to node.js format

---
+++ app.js
@@ -0,0 +1,47 @@
+var express = require("express");
+//includes all the content inside express
+var app = express();
+app.set("views", __dirname + "/views");
+app.engine("html", require("ejs").renderFile);
+app.use(express.static(__dirname + "/public"));
+var colors = {
+    yellow: '#ff0',
+    green: '#0f0',
+    red: '#f00',
+    blue: '#00f',
+    gray: '#888',
+    maroon: '#800',
+    olive: '#880',
+    cyan: '#0ff',
+    teal: '#088',
+    navy: '#008',
+    fuchsia: '#f0f',
+    orange: '#f80',
+    brown: '#844',
+    purple: '#808',
+    plum: '#804'
+  };
+  
+var nearestColor = require('nearest-color').from(colors);
+
+app.get("/nearestColor/:id", function(req, res) {
+    
+    var match = nearestColor('#' + req.params.id);
+    console.log(match);
+    var data = match.value.substring(1);
+    res.header("Content-Type",'application/json');
+    res.header("Access-Control-Allow-Origin",'*');
+    res.send(JSON.stringify(data));
+});
+
+app.get("/", function(req, res) {
+    res.render("index.html");
+})
+
+app.get("/chromkey", function(req, res) {
+    res.render("chroma-key-demo.html");
+})
+
+app.listen(6789, 'localhost', function(){
+    console.log("The server has started");
+});
---
+++ public/js/index.js
@@ -0,0 +1,68 @@
+var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-2196019-1']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+	
+ function httpGet(theUrl) {
+		var xmlHttp = new XMLHttpRequest();
+		xmlHttp.open( "GET", theUrl, false); // false for synchronous request
+		xmlHttp.send(null);
+		return xmlHttp.responseText;
+	}
+	
+	function componentToHex(c) {
+	var hexNum = parseInt(c);
+		var hex = hexNum.toString(16);
+		return hex.length == 1 ? "0" + hex : hex;
+	}
+
+	function rgbToHex(r, g, b) {
+		return componentToHex(r) + componentToHex(g) + componentToHex(b);
+	}
+	
+	function getNearestColor(r, g, b) {
+		var hexCode = rgbToHex(r, g, b);
+		return JSON.parse(httpGet("http://localhost:6789/nearestColor/"+hexCode));
+	}	
+	
+	var timer = setInterval(check, 100);
+	
+	function check() {
+		if ($('#first-title').attr('red') != null) {
+			clearInterval(timer);
+			displayNearestColor();
+		}
+	}
+	var foundNearest = false;
+	function displayNearestColor() {
+		var red = $('#first-title').attr('red');
+		var green = $('#first-title').attr('green');
+		var blue = $('#first-title').attr('blue');
+		var nearest = "#" + getNearestColor(red, green, blue);
+		$("#nearest h3").append(nearest);
+		$("#nearest-color").css('background', nearest);
+		foundNearest = true;
+	}
+	
+	$("body").on('click', '#nearest h3', function(event) {
+		if (foundNearest == false) {
+			foundNearest = true;
+			var red = $('#first-title').attr('red');
+			var green = $('#first-title').attr('green');
+			var blue = $('#first-title').attr('blue');
+			var nearest = "#" + getNearestColor(red, green, blue);
+			$("#nearest h3").append(nearest);
+			$("#nearest-color").css('background', nearest);
+		}
+	});
+
+	$("body").on('click', '.changeColor', function(event) {
+		localStorage.red = $(this).attr('red');
+		localStorage.green = $(this).attr('green');
+		localStorage.blue = $(this).attr('blue');
+	});
